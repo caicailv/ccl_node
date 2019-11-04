881be671e5227015d5a5bc333c9c2ef3991c7078
张海瑞 <1401394657@qq.com>
2019-11-04 19:03:19 +0800

    2019-11-4

--- router.js
+++ router.js
@@ -56,8 +56,63 @@ router.get('/v1/album', function (req, res) {
                 id: 10,
                 title: "今天和傻屌出去玩",
                 img_scr: 'http://localhost:3001/public/img/10.jpg'
-            }]
+            },
+            {
+                id: 11,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 12,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 13,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 14,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 15,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 16,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 17,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 18,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 19,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 20,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+            {
+                id: 21,
+                title: "今天和傻屌出去玩",
+                img_scr: 'http://localhost:3001/public/img/10.jpg'
+            },
+        ]
     )
-
 });
 module.exports = router; 
