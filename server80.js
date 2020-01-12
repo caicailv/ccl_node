1f8dfbd6baefe7acc15a7ff5f4977a50aa528083
caicailv <1401394657@qq.com>
2020-01-12 20:33:57 +0800

    2020-01-12

---
+++ server80.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+var fs = require("fs");
+let path = require('path');
+let severLocalhost = 8080;
+app.use('/static', express.static(path.join(__dirname, './dist/static')));
+app.get('/', function (req, res) {
+    fs.readFile("./dist/index.html", function (err, data) {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+        }
+        else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data.toString());
+        }
+        res.end();
+
+    });
+
+})
+app.listen(severLocalhost, function () {
+    console.log('ccl_blog服务器启动成功' + severLocalhost);
+})
